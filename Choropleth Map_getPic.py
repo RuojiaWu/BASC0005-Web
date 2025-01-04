989c4676bbec643348ee089ae28ac759e2859101
RuojiaWu <uclqrwu@ucl.ac.uk>
2025-01-04 14:12:12 +0800

    Update and rename 2_getPic.py to Choropleth Map_getPic.py

--- Choropleth Map_getPic.py
+++ Choropleth Map_getPic.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-
-
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from os import path
@@ -10,8 +7,8 @@ import time
 matplotlib.use('TkAgg')
 
 if __name__ == '__main__':
-    # 读取Shapefile数据
-    shapefile_path = r'D:\self\202412\china\final.shp'  # 替换为你的Shapefile路径
+    # read Shapefile
+    shapefile_path = r'D:\self\202412\china\final.shp'  
     picPath = r'D:\self\202412\china\pic'
     gdf = gpd.read_file(shapefile_path)
 
@@ -32,20 +29,19 @@ if __name__ == '__main__':
                   'income2021', 'income2022', 'edu2020', 'edu2021', 'edu2022', 'shiye2020', 'shiye2021', 'shiye2022',
                   'pov2020', 'pov2021', 'pov2022']:
         print(field)
-        # 创建地图
+        # create map
         fig, ax = plt.subplots(1, 1, figsize=(12, 8))
         gdf.boundary.plot(ax=ax, linewidth=1, color='black')
         gdf.plot(column=field, ax=ax, legend=True,
                  cmap='Reds', missing_kwds={"color": "lightgrey", "label": "No data"})
 
-        colorbar = ax.get_figure().axes[-1]  # 获取最后一个axes，即colorbar
-        colorbar.set_title(units[num], rotation=0, pad=15)  # 设置标题水平，并增加一些padding
+        colorbar = ax.get_figure().axes[-1]  
+        colorbar.set_title(units[num], rotation=0, pad=15) 
 
-        # 添加标题
+        # add titles
         plt.title(titles[num], fontsize=15)
-        plt.axis('off')  # 关闭坐标轴
+        plt.axis('off')  
         plt.savefig(path.join(picPath, "{}.jpg".format(field)), dpi=1000)
-        # plt.savefig(path.join(picPath, "{}.jpg".format(field)), dpi=300)
 
         num += 1
 
